2fd17906c2a64da4342a06415ee5093e66ce0ab2
Sanath Kumar Matrubai <31627627+msssanath@users.noreply.github.com>
2018-03-08 22:50:44 +0530

    Add files via upload
    #3

--- generalSearch.py
+++ generalSearch.py
@@ -1,64 +1,73 @@
 import requests,bs4
 
-x='https://www.flipkart.com/search?q='
-y='&otracker=start&as-show=on&as=off'
 
-print("Input the search string")
-search=input()
+def generalS(search):
 
-res = requests.get(x+str(search)+y)
-res.raise_for_status()
+    '''This method takes the Search String as input and returns the first 5 results of 
+       the string in Flipkart, Amazon and Snapdeal.''' 
 
-soup1 = bs4.BeautifulSoup(res.text,"html.parser")
-soup2 = bs4.BeautifulSoup(res.text,"html.parser")
-names = soup1.find_all("a",{"class":"_2cLu-l"})
-prices = soup2.find_all("div",{"class":"_1vC4OE"})
+    x='https://www.flipkart.com/search?q='
+    y='&otracker=start&as-show=on&as=off'
 
+    res = requests.get(x+str(search)+y)
+    res.raise_for_status()
 
-print("Flipkart's results")
-count = 0
-for name,price in zip(names,prices):
-    if(count<5):
-        print(name.get_text() + "\t" + "Rs" + price.get_text())
-        count=count+1
-    else:
-        break
+    soup1 = bs4.BeautifulSoup(res.text,"html.parser")
+    soup2 = bs4.BeautifulSoup(res.text,"html.parser")
+    names = soup1.find_all("a",{"class":"_2cLu-l"})
+    prices = soup2.find_all("div",{"class":"_1vC4OE"})
 
-
-x = 'https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Delectronics&field-keywords='
-res = requests.get(x+str(search))
-res.raise_for_status()
-
-soup1 = bs4.BeautifulSoup(res.text,"html.parser")
-soup2 = bs4.BeautifulSoup(res.text,"html.parser")
-names = soup1.find_all("a",{"class":"a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal"})
-prices = soup1.find_all("span",{"class":"a-size-base a-color-price s-price a-text-bold"})
-print("Amazon's results")
-count=0
-for name,price in zip(names,prices):
-    if(count<5):
-        print(name.get_text() + "\t" + "Rs" + price.get_text())
-        count=count+1
-    else:
-        break
+    results = ""
+    results = results + "Flipkart's results : \n"
+    count = 0
+    for name,price in zip(names,prices):
+        if(count<5):
+            results = results + name.get_text() + "\t" + "Rs" + price.get_text() + "\n"
+            count=count+1
+        else:
+            break
 
 
-x = 'https://www.snapdeal.com/search?keyword='
-res = requests.get(x+str(search))
-res.raise_for_status()
+    x = 'https://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Delectronics&field-keywords='
+    res = requests.get(x+str(search))
+    res.raise_for_status()
 
-soup1 = bs4.BeautifulSoup(res.text,"html.parser")
-soup2 = bs4.BeautifulSoup(res.text,"html.parser")
-names = soup1.find_all("p",{"class":"product-title"})
-prices = soup2.find_all("span",{"class":"lfloat product-price"})
+    soup1 = bs4.BeautifulSoup(res.text,"html.parser")
+    soup2 = bs4.BeautifulSoup(res.text,"html.parser")
+    names = soup1.find_all("a",{"class":"a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal"})
+    prices = soup1.find_all("span",{"class":"a-size-base a-color-price s-price a-text-bold"})
+    results = results + "Amazon's results : " + "\n"
+    count=0
+    for name,price in zip(names,prices):
+        if(count<5):
+            results = results + name.get_text() + "\t" + "Rs" + price.get_text() + "\n"
+            count=count+1
+        else:
+            break
 
-print("Snapdeal's results")
-count=0
-for name,price in zip(names,prices):
-    if(count<5):
-        print(name.get_text() + "\t" + price.get_text())
-        count = count+1
-    else:
-        break
 
+    x = 'https://www.snapdeal.com/search?keyword='
+    res = requests.get(x+str(search))
+    res.raise_for_status()
 
+    soup1 = bs4.BeautifulSoup(res.text,"html.parser")
+    soup2 = bs4.BeautifulSoup(res.text,"html.parser")
+    names = soup1.find_all("p",{"class":"product-title"})
+    prices = soup2.find_all("span",{"class":"lfloat product-price"})
+
+    results = results + "Snapdeal's results : " + "\n"
+    count=0
+    for name,price in zip(names,prices):
+        if(count<5):
+            results = results + name.get_text() + "\t" + price.get_text() + "\n"
+            count = count+1
+        else:
+            break
+    return results
+
+if __name__ == '__main__':
+    print("Input the search string")
+    SearchString = input()
+    results = ""
+    results = generalS(SearchString)
+    print(results)
